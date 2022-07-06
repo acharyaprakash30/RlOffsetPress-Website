faf70520cf82cf48f62c43f8026b24025d7a3f2a
bijayneupane18 <bjneupane5@gmail.com>
2022-07-06 17:28:18 +0545

    image carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ImageCarousel from "./ImageCarousel";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <ImageCarousel/>
     </div>
   );
 }
