30378daf7d11dfeb9fdcb83940866a338fbc8d27
bijayneupane18 <bjneupane5@gmail.com>
2022-07-21 02:02:46 +0545

    complete navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -63,8 +63,8 @@ const Navbar = () => {
         </ul>
         <NavLink to='/blogs'><li onClick={()=>showMenu(true)} className='navbar-li'>Blogs</li></NavLink>
         <NavLink to='/contacts'><li onClick={()=>showMenu(true)} className='navbar-li'>Contacts</li></NavLink>
-      </ul>
-      <ul className='navbar-address'>
+       </ul>
+      <ul className={menu?'navbar-address':'menu-address'}>
         <a href='mailto:rloffsetpress@gmail.com'><li className='navbar-add-mail'><svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mt-1 mr-1 color-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
         </svg>rloffsetpress@gmail.com</li></a>
